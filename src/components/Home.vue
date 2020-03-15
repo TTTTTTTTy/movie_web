332e8673b6ff5829f1d22cbb718e11eba3e25322
汤烨春 <tangyechun@harmonycloud.com.cn>
2020-03-15 16:42:30 +0800

    project

---
+++ src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div >
+    <div>
+      <img src="@/assets/logo.png" class="pic">
+    </div>
+
+    <el-input style="width: 200px" ></el-input>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: "Home"
+  }
+</script>
+
+<style scoped>
+  .pic{
+    height: 180px;
+  }
+
+  .body{
+    position: relative;
+    top: 50%;
+  }
+
+</style>
